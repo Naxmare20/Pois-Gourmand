cab47783e369819550ae2b9a1a78ae36477cf751
Naxmare20 <nath.favereaux@gmail.com>
2023-08-22 15:57:02 +0200

    first

---
+++ index.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="Naxmare">
+    <title>Au point gourmant</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Srisakdi&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/normalize.css">
+    <link rel="stylesheet" href="assets/css/Style.css">
+</head>
+<body>
+    <header>
+        <i class="fa-solid fa-kiwi-bird icon-site"></i>
+        <h1 class="Titre-Header">Au Pois Gourmand</h1>
+    </header>
+    <main>
+        <article>
+            <img src="assets/img/rome.jpg" alt="">
+            <h2>Formule Rome</h2>
+            <p class="text-article">Tomates buratina</p>
+            <p class="text-article">Rizotto aux asperges</p>
+            <p class="text-article">Panna cotta</p>
+            <a href="views/resultat2.php?parametre=valeur"><button class="Color-Cyan">Choisir</button></a>
+        </article>
+        <article>
+            <img src="assets/img/nyork.jpg" alt="">
+            <h2>Formule New-York</h2>
+            <p class="text-article">César salade</p>
+            <p class="text-article">Cheese burger</p>
+            <p class="text-article">Cheesecake</p>
+            <a href="views/resultat3.php?parametre=valeur"><button class="Color-Red">Choisir</button></a>
+
+        </article>
+        <article>
+            <img src="assets/img/delhi.jpg" alt="">
+            <h2>Formule Delhi</h2>
+            <p class="text-article">Poppadoms</p>
+            <p class="text-article">Agneau byriani</p>
+            <p class="text-article">Lassi mangue</p>
+            <a href="views/resultat4.php?parametre=valeur"><button class="Color-Yellow">Choisir</button></a>
+        </article>
+        <article>
+            <img src="assets/img/hanoi.jpg" alt="">
+            <h2>Formule Hanoî</h2>
+            <p class="text-article">Nems aux crevettes</p>
+            <p class="text-article">Poulet saté</p>
+            <p class="text-article">Perles de coco</p>
+            <a href="views/resultat5.php?parametre=valeur"><button class="Color-blue">Choisir</button></a>
+        </article>
+    </main>
+    <footer>
+        <div class="Petit-Oiseau">
+            <i class="fa-solid fa-kiwi-bird"></i>
+            <p class="Petit-Pois">.....</p>
+            <i class="fa-solid fa-kiwi-bird"></i>
+            <p class="Petit-Pois">....</p>
+            <i class="fa-solid fa-kiwi-bird"></i>
+            <p class="Petit-Pois">...</p>
+            <i class="fa-solid fa-kiwi-bird"></i>
+            <p class="Petit-Pois">..</p>
+            <i class="fa-solid fa-kiwi-bird"></i>
+            <p class="Petit-Pois">.</p>
+            <h3 class="sous-titre-footer">Au Pois Gourmand</h3>
+        </div>
+    </footer>
+    <script src="https://kit.fontawesome.com/46e6162e9a.js" crossorigin="anonymous"></script>
+    <script src="assets/js/script.js"></script>
+</body>
+</html>
+
+
+
+<?php
---
+++ views/resultat3.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Resultat2</title>
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Srisakdi&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="../assets/css/normalize.css">
+  <link rel="stylesheet" href="../assets/css/Style2.css">
+</head>
+<body>
+<header>
+  <i class="fa-solid fa-kiwi-bird icon-site"></i>
+  <h1 class="Titre-Header">Au Pois Gourmand</h1>
+</header>
+<main>
+  <article class="BlockMain">
+    <h2>Merci pour votre commande !</h2>
+    <img class="Image-command" src="../assets/img/nyork.jpg" alt="">
+    <div class="Sous-BlockMain">
+      <div class="Information-Formule">
+        <h3 class="TitreFormule">Votre formule New-York Arrive dans quelques instants...</h3>
+        <i class="fa-solid fa-kiwi-bird birdh3"></i>
+      </div>
+      <p>Nous vous souhaitons une bonne dégustation</p>
+      <p>un café gourmand vous est proposé pour terminez votre pause gourmande en douceur</p>
+      <p class="Facture">-- Votre facture sera de 23 €</p>
+      <a href="../index.php"><button>Choisir un autre menu</button></a>
+    </div>
+  </article>
+  <article class="BlockMain2">
+    <div class="TextBlockMain2">
+      <p> -- Vous avez aimé ?</p>
+      <i class="fa-solid fa-star StarGray"></i>
+      <i class="fa-solid fa-star"></i>
+      <i class="fa-solid fa-star"></i>
+      <i class="fa-solid fa-star"></i>
+      <i class="fa-regular fa-star"></i>
+    </div>
+    <img class="ImageBlockMain2" src="../assets/img/pg.jpg" alt="">
+  </article>
+
+</main>
+<footer>
+  <div class="Petit-Oiseau">
+    <i class="fa-solid fa-kiwi-bird"></i>
+    <p class="Petit-Pois">.....</p>
+    <i class="fa-solid fa-kiwi-bird"></i>
+    <p class="Petit-Pois">....</p>
+    <i class="fa-solid fa-kiwi-bird"></i>
+    <p class="Petit-Pois">...</p>
+    <i class="fa-solid fa-kiwi-bird"></i>
+    <p class="Petit-Pois">..</p>
+    <i class="fa-solid fa-kiwi-bird"></i>
+    <p class="Petit-Pois">.</p>
+    <h3 class="sous-titre-footer">Au Pois Gourmand</h3>
+  </div>
+</footer>
+<script src="https://kit.fontawesome.com/46e6162e9a.js" crossorigin="anonymous"></script>
+</body>
+</html>
+
+<?php
+if (isset($_GET['parametre'])) {
+    $valeur = $_GET['parametre'];
+    echo "Le paramètre passé est : $valeur";
+} else {
+    echo "Aucun paramètre n'a été passé dans l'URL.";
+}
+?>
